642fa0f6abb5345d59a5ef748243b66ef63acbb1
takalkarsayali <sayali27.takalkar@gmail.com>
2022-06-07 16:49:42 +0530

    Let's Play

---
+++ grid.js
@@ -0,0 +1,12 @@
+const GRID_SIZE = 21
+
+export function randomGridPostion() {
+    return {
+        x: Math.floor(Math.random() * GRID_SIZE) + 1,
+        y: Math.floor(Math.random() * GRID_SIZE) + 1
+    }
+}
+
+export function outSideGrid(position){
+    return (position.x < 1 || position.x > GRID_SIZE || position.y < 1 || position.y > GRID_SIZE)
+}
